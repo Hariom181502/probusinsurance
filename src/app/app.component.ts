31d155cf6d0730e4c6d2113c040a49961dc798de
Hariom <hariomkumawat786@gmail.com>
2025-03-22 10:02:58 +0530

    implement 19 version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
