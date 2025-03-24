c4840706b3d4dadd492294b660edd183de3f4793
deepakjain <deepakjain1231997@gmail.com>
2025-03-24 18:57:01 +0530

    quotation single card working

--- src/app/quotation-list/quotation-list.component.ts
+++ src/app/quotation-list/quotation-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SingleQuotationComponent } from "./single-quotation/single-quotation.component";
 import { InnerHeaderComponent } from "../layout/inner-header/inner-header.component";
+import { MaterialModule } from '../shared/module/material/material.module';
 
 @Component({
   selector: 'app-quotation-list',
-  imports: [SingleQuotationComponent, InnerHeaderComponent],
+  imports: [SingleQuotationComponent, InnerHeaderComponent, MaterialModule],
   templateUrl: './quotation-list.component.html',
   styleUrl: './quotation-list.component.scss'
 })
--- src/app/quotation-list/single-quotation/single-quotation.component.ts
+++ src/app/quotation-list/single-quotation/single-quotation.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../shared/module/material/material.module';
 
 @Component({
   selector: 'app-single-quotation',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './single-quotation.component.html',
   styleUrl: './single-quotation.component.scss'
 })
