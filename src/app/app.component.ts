6b26a13c961ceb8abadef0bee344224859e4492c
deepakjain <deepakjain1231997@gmail.com>
2025-03-24 14:44:02 +0530

    outer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
 import { MaterialModule } from './shared/module/material/material.module';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, MaterialModule],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { QuotationListComponent } from './quotation-list/quotation-list.component';
 
 export const routes: Routes = [
-    {path:'home',component:HomeComponent}
+    {path:'home',component:HomeComponent},
+    {path:'quotation',component:QuotationListComponent},
 ];
