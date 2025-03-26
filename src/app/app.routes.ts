45f3110a2fbacbdc8d74a4ec9c4b7c448a981a36
Hariom <hariomkumawat786@gmail.com>
2025-03-26 12:21:49 +0530

    working on policy details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { QuotationListComponent } from './quotation-list/quotation-list.component';
 import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
+import { PolicyDetailsComponent } from './policy-details/policy-details.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'home',component:HomeComponent},
     {path:'quotation',component:QuotationListComponent},
     {path:'vehicleDetails',component:VehicleDetailsComponent},
+    {path:'policyDetails',component:PolicyDetailsComponent},
 ];
