15cf11de7f30cd6aba84fae81cb8abcd259eb6a9
Hariom181502 <152277446+Hariom181502@users.noreply.github.com>
2025-04-03 23:38:48 +0530

    changes in all comps

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MaterialModule } from '../shared/module/material/material.module';
 import { HeaderComponent } from "../layout/header/header.component";
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +9,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-   
-  regForm!:FormGroup;
+export class HomeComponent {
 
-  constructor(private _router:Router,private _fb:FormBuilder){}
-
-  ngOnInit(): void {
-   this.initializeForm()  
-  }
+  constructor(private _router:Router){}
 
   getVehicelDtl(){
     this._router.navigate(['vehicleDetails']);    
   }
 
-  initializeForm(){
-   this.regForm = this._fb.group({
-    regNum:['',Validators.required]
-   })
-  }
 }
